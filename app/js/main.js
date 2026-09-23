5e750b1874da88ddba392551080cf7f0982aa631
Eissayou <eissayouj@gmail.com>
2026-09-22 19:43:13 -0500

    Replace Carto tile layer with OpenStreetMap and update CSP

--- app/js/main.js
+++ app/js/main.js
@@ -215,11 +215,10 @@ function initMap() {
         maxBoundsViscosity: 1.0
     });
 
-    // Dark theme basemap (CartoDB Dark Matter)
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 20
+    // OpenStreetMap basemap
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     markerClusterGroup = L.markerClusterGroup({
